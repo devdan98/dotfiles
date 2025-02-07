25bcbc78e5ba5b007216548cdd962e9471d88ac2
devdan98 <dan.reid3005@gmail.com>
2025-02-07 20:32:03 +0000

    fixed telescope

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,10 +1,18 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  -- or                              , branch = '0.1.x',
-  dependencies = { "nvim-lua/plenary.nvim" },
-  vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" }),
-  vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }),
-  vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" }),
-  vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" }),
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      vim.keymap.set("n", "<space>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require("telescope.builtin").find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
+      end)
+    end,
+  },
 }
